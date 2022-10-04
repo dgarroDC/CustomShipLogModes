1a2db751b4f199260e7ce805960394aa8cfe9493
Damián Garro <dgarro@dc.uba.ar>
2022-10-03 22:58:43 -0300

    Revert "remove test modes, release commit?"
    This reverts commit f2f8346ee0899cd497d0f2a3604c44fccba23b03.

---
+++ CustomShipLogModes/ModeA.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace CustomShipLogModes;
+
+public class ModeA : ShipLogMode
+{
+    private OWAudioSource _oneShotSource;
+
+    public override void Initialize(ScreenPromptList centerPromptList, ScreenPromptList upperRightPromptList, OWAudioSource oneShotSource)
+    {
+        _oneShotSource = oneShotSource;
+    }
+
+    public override void EnterMode(string entryID = "", List<ShipLogFact> revealQueue = null)
+    {
+        _oneShotSource.PlayOneShot(AudioType.TravelerPrisoner);
+    }
+
+    public override void ExitMode()
+    {
+        _oneShotSource.Stop();
+    }
+
+    public override void OnEnterComputer()
+    {
+    }
+
+    public override void OnExitComputer()
+    {
+    }
+
+    public override void UpdateMode()
+    {
+    }
+
+    public override bool AllowModeSwap()
+    {
+        return true;
+    }
+
+    public override bool AllowCancelInput()
+    {
+        return true;
+    }
+
+    public override string GetFocusedEntryID()
+    {
+        return "";
+    }
+}
---
+++ CustomShipLogModes/ModeB.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace CustomShipLogModes;
+
+public class ModeB : ShipLogMode
+{
+    private OWAudioSource _oneShotSource;
+
+    public override void Initialize(ScreenPromptList centerPromptList, ScreenPromptList upperRightPromptList, OWAudioSource oneShotSource)
+    {
+        _oneShotSource = oneShotSource;
+    }
+
+    public override void EnterMode(string entryID = "", List<ShipLogFact> revealQueue = null)
+    {
+        _oneShotSource.PlayOneShot(AudioType.TravelerNomai);
+    }
+
+    public override void ExitMode()
+    {
+        _oneShotSource.Stop();
+    }
+
+    public override void OnEnterComputer()
+    {
+    }
+
+    public override void OnExitComputer()
+    {
+    }
+
+    public override void UpdateMode()
+    {
+    }
+
+    public override bool AllowModeSwap()
+    {
+        return true;
+    }
+
+    public override bool AllowCancelInput()
+    {
+        return true;
+    }
+
+    public override string GetFocusedEntryID()
+    {
+        return "";
+    }
+}
